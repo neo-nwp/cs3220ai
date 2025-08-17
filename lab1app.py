fbc27e922654f7e2728c1541fa87aea2d5174608
Hanna2110 <142014166+Hanna2110@users.noreply.github.com>
2025-08-16 22:16:05 -0600

    Update lab1app.py

--- lab1app.py
+++ lab1app.py
@@ -79,6 +79,7 @@ def main():
         df_select = df_select.reset_index(drop=True)
         st.dataframe(df_select, hide_index=True)
         nodes,edges,edges_width=setGraphData(df_select)
+        st.text(nodes)
         flight_net=buildGraph(nodes,edges,edges_width)
         flight_net.save_graph('L1_Network_of_flights.html')
         HtmlFile = open(f'L1_Network_of_flights.html', 'r', encoding='utf-8')
