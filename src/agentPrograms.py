34dfd6d718859ac4acdaedd2f73debf21bfb34ca
Hanna Yehoshyna <hanna.yehoshyna1982@gmail.com>
2025-09-17 17:03:25 -0600

    add import random to AP.py

--- src/agentPrograms.py
+++ src/agentPrograms.py
@@ -1,3 +1,5 @@
+import random
+
 '''An idea of Random Agent Program is to choose an action at random, ignoring all percepts'''
 def RandomAgentProgram(actions):
    return lambda percept: random.choice(actions)
